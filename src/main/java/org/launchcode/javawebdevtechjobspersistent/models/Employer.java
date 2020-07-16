04a9a25ac6735829fe16df850a3f3f9c390a8352
Teresa Keller <56941889+tkeller333@users.noreply.github.com>
2020-07-16 15:45:49 -0500

    working

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -3,12 +3,16 @@ package org.launchcode.javawebdevtechjobspersistent.models;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
 
+    @NotBlank(message = "Please enter a location.")
+    @Size(max = 500, message = "Location must be less than 500 characters.")
     private String location;
 
     @OneToMany
