35c44c48c2afa197b56aa256dcdb63e8e63fd921
Teresa Keller <56941889+tkeller333@users.noreply.github.com>
2020-07-16 19:46:26 -0500

    almost

--- queries.sql
+++ queries.sql
@@ -15,6 +15,11 @@ WHERE employer.location = "St. Louis City";
 
 ## Part 3: Test it with SQL
 
-DROP table assignment4.job;
+DROP table job;
 
 ## Part 4: Test it with SQL
+
+SELECT job.name, skill.description
+FROM skill, job
+INNER JOIN job_skills ON job.id = job_skills.jobs_id
+WHERE skill.id = job_skills.skills_id;
